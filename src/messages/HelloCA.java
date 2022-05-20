b7639ace44dbc4658802c1cf6a931778ff8576a2
Muammer Diri <muammerdiri2@gmail.com>
2022-05-20 10:29:00 +0300

    Created Hello Commant Message

---
+++ src/messages/HelloCA.java
@@ -0,0 +1,26 @@
+package messages;
+
+import java.nio.ByteBuffer;
+
+public class HelloCA implements IHelloCA{
+    @Override
+    public byte[] successResponseMessage(byte opcode, int dataLength, byte[] data) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] commentMessage(byte opcode, int dataLength, byte[] data) {
+        //Main array = { header +opcode + Length + Data }
+        byte [] responseData ={tools.ProtocolName.COMMAND_HEADER,tools.ProtocolName.HELLO,0x00};
+
+        short length = (short)dataLength;
+        byte[] lengthToByte = ByteBuffer.allocate(2).putShort(length).array();
+        responseData =tools.Tools.byteArrayConcatenation(responseData,lengthToByte);
+
+        responseData= tools.Tools.byteArrayConcatenation(responseData,data);
+        return responseData;
+    }
+
+
+
+}
