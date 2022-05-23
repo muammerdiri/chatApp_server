877217aa28d7511728af7baf2ec3219c2f63a33e
Muammer Diri <muammerdiri2@gmail.com>
2022-05-23 09:04:00 +0300

    HelloCA methods have been created.

--- src/messages/HelloCA.java
+++ src/messages/HelloCA.java
@@ -3,24 +3,22 @@ package messages;
 import java.nio.ByteBuffer;
 
 public class HelloCA implements IHelloCA{
+    
     @Override
     public byte[] successResponseMessage(byte opcode, int dataLength, byte[] data) {
         return new byte[0];
     }
 
     @Override
-    public byte[] commantMessage(byte opcode, int dataLength, byte[] data) {
+    public byte[] commentMessage(byte[] data) {
         //Main array = { header +opcode + Length + Data }
         byte [] responseData ={tools.ProtocolName.COMMAND_HEADER,tools.ProtocolName.HELLO,0x00};
 
-        short length = (short)dataLength;
+        short length = (short)data.length;
         byte[] lengthToByte = ByteBuffer.allocate(2).putShort(length).array();
         responseData =tools.Tools.byteArrayConcatenation(responseData,lengthToByte);
 
         responseData= tools.Tools.byteArrayConcatenation(responseData,data);
         return responseData;
     }
-
-
-
 }
