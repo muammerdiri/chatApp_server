d8f16819f265d2c94cf94067add5dcb602e7b28d
Muammer Diri <muammerdiri2@gmail.com>
2022-05-23 09:02:13 +0300

    method parameters edited

--- src/messages/IHelloCA.java
+++ src/messages/IHelloCA.java
@@ -1,10 +1,6 @@
 package messages;
 
 public interface IHelloCA  {
-
         byte[] successResponseMessage(byte opcode, int dataLength, byte[] data);
-        byte[] commantMessage(byte opcode, int dataLength, byte[] data);
-
-
-
+        byte[] commentMessage( byte[] data);
 }
