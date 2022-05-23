55df92318f4e0b96903c408aad78b70d425cf9bd
Muammer Diri <muammerdiri2@gmail.com>
2022-05-23 09:33:39 +0300

    Signature Script method converted from string type to byte array type

--- src/messages/SignaturePublicKey.java
+++ src/messages/SignaturePublicKey.java
@@ -1,5 +1,8 @@
 package messages;
 
+import tools.Tools;
+
+import java.nio.ByteBuffer;
 import java.security.PrivateKey;
 import java.security.Signature;
 import java.util.Base64;
@@ -8,20 +11,23 @@ public class SignaturePublicKey implements ISignaturePublicKey {
 
 
     @Override
-    public String signatureCommantMessage(byte [] clientPublicKey, PrivateKey serverPrivateKey)  {
+    public byte [] signatureCommantMessage(byte [] clientPublicKey, PrivateKey serverPrivateKey)  {
 
         byte[] signature = null;
         try {
             Signature privateSignature = Signature.getInstance("SHA256withRSA");
-
             privateSignature.initSign(serverPrivateKey);
             privateSignature.update(clientPublicKey);
-
             signature = privateSignature.sign();
+            byte[] lengthToByte = ByteBuffer.allocate(2).putShort((short)signature.length).array();
+            byte [] responseData ={tools.ProtocolName.COMMAND_HEADER,tools.ProtocolName.HELLO,0x00};
+            responseData = Tools.byteArrayConcatenation(responseData,lengthToByte);
+            signature = Tools.byteArrayConcatenation(responseData,signature);
+
         }catch (Exception e){
             e.printStackTrace();
         }
-        return Base64.getEncoder().encodeToString(signature);
+        return signature;
 
     }
 
